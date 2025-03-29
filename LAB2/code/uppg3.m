f063dd75abe4e5d42877ddb2f5358a7a4cb490b2
Tristan Casper Erasmus Harland Peterson <tcehp@kth.se>
2025-03-29 20:48:50 +0100

    Added code for parts b and c

--- LAB2/code/uppg3.m
+++ LAB2/code/uppg3.m
@@ -23,18 +23,22 @@ function [Q, R] = gramSchmidt(A)
     end
 end
 
-A = [ones(N, 1), tt];
-b = X(:);
-[testQ , testR] = gramSchmidt(A);
+%[testQ , testR] = gramSchmidt(A_linear);
 
-function x = leastSquares(A, b)
+function [x, C] = leastSquares(A, b)
+    ATA = (A')*A;
+    x = ATA \ (A')*b;
+    C = cond(ATA);
+end
+
+function [x, C] = leastSquaresQR(A, b)
     [Q, R] = qr(A, 0); % Second parameter indicates we want the reduced factorization, 
     %   which saves computing power since we onlt need Q to have the basis elements
     %   for the basis for the column space of A (n=2) rather than the entire space 
     %   which is isomorphic to R^(m*m) (m=big number).
 
     x = R \ (Q') * b; %  Back substitution for QR variation of normal equations
-    
+    C = cond(R);
     %[m, n] = size(A);
     %R_hat = R(1:n,1:n);
     %d = (Q') * b;
@@ -56,6 +60,7 @@ function plotSeries(X, tt, linear_model)
     xlabel('Time (days)');
     ylabel('USD-SEK');
     hold off;
+    waitfor(gcf);
     % Er kod här...
 end
 
@@ -64,11 +69,37 @@ function MSE = MSE(X, Y)
     MSE = (1/N)*sum((X-Y).^2);
 end
 
-linear_coeffs = leastSquares(A, b);
-linear_model = linear_coeffs(1) + linear_coeffs(2) * tt;
-plotSeries(X, tt, linear_model);
-fprintf('Linear model:\ty = %f + %f*t\n', linear_coeffs);
-fprintf('Mean squared error: %f\n', MSE(X, linear_model))
+b = X(:);
+
+A_linear = [ones(N, 1), tt];
+
+L = 500;
+A_periodic = zeros(N, 4);
+for t = 1:N
+    A_periodic(t, :) = [1, t, sin(2*pi*t/L), cos(2*pi*t/L)];
+end
+
+
+
+%[linear_coeffs, cond_num] = leastSquares(A_linear, b);
+%[linear_coeffs_QR, cond_num_QR] = leastSquaresQR(A_linear, b);
+
+[periodic_coeffs, periodic_cond_num] = leastSquaresQR(A_periodic, b);
+
+%disp(linear_coeffs);
+%disp(linear_coeffs_QR);
+%disp(condition_num);
+%disp(condition_num_QR);
+
+%linear_model = linear_coeffs(1) + linear_coeffs(2) * tt;
+periodic_model = periodic_coeffs(1)  ...
+    + periodic_coeffs(2) * tt ...
+    + periodic_coeffs(3) * sin(2*pi*tt/L) ...
+    + periodic_coeffs(4) * cos(2*pi*tt/L);
+%plotSeries(X, tt, linear_model);
+plotSeries(X, tt, periodic_model);
+%fprintf('Linear model:\ty = %f + %f*t\n', linear_coeffs);
+%fprintf('Mean squared error: %f\n', MSE(X, linear_model))
 
 
 %% 3b Normalekvationerna och konditionering
