e2d9c540bfa234edbdc3982f266e96aacd17c9a1
04lucgra <lucas.grant@gmail.com>
2025-04-04 03:55:44 +0200

    plop

--- LAB2/code/uppg4.m
+++ LAB2/code/uppg4.m
@@ -1,34 +1,47 @@
+%% Assignment a)
 
 
-%% 4a Konvergensstudie
-% Er kod här...
+function y = func1(x)
+    y = 2/(sqrt(pi))*exp(-x^2)
+end
+
+interval_start
+
+x = 1
+y = erf(x)
+
+plot()
+
+comparison_value = erf()
 
 
 
+function I = trapets(a, b, g, N)
+    % a: Start of the interval
+    % b: End of the interval
+    % g: Function handle to integrate
+    % N: Number of subintervals
 
+    step_length = (b - a) / N;
+    x = linspace(a, b, N + 1);
+    y = g(x);
 
-
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-function I=trapets(N)
-
-%  Indata:
-%
-%  N  - antal delintervall (skalär)
-%
-%  Utdata:
-%
-%  I - integralen (skalär)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% Er kod här...
-
+    I = step_length * (sum(y) - (y(1) + y(end)) / 2);
 
 end
 
 
+
+f = trapets(0, 1, @func1, N);
+
+
+
+%% Assignment b)
+
+
+
+
+%% Assignment c)
 
 
 
