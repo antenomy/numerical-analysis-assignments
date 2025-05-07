ed53498e824b165c158182ae6eed57c7c2e86107
Tristan Casper Erasmus Harland Peterson <tcehp@kth.se>
2025-05-07 09:30:29 +0200

    Part 3 question 4 finished

--- Project/code/part3.m
+++ Project/code/part3.m
@@ -1,3 +1,5 @@
+format long
+
 omega = 19;
 
 S0 = @(r) cos(24*sqrt(r)).*exp(-900*r);
@@ -49,57 +51,143 @@ function I = numInt(s, integrand)
     end
 end
 
-eta = trapInt2D(100, nuIntegrand, omega);
-disp(eta);
-
-[B,Sol] = hhsolver(omega,S,100); 
-
-
-
-function r = func_r(x, N, eta, omega, alphas)
+function r = func_r(x, N, eta, omega, alphas, Ic_alphas)
     r = zeros(N, 1);
     for j = 1:N
-        r(t) = x(1)*eta*cos(omega*x(2)*cos(alphas(j)) + omega*x(3)*sin(alphas(j)));
+        r(j) = x(1)*eta*cos(omega*x(2)*cos(alphas(j)) + omega*x(3)*sin(alphas(j))) - Ic_alphas(j);
     end
 end
 
 function Dr = derivative_r(x, N, eta, omega, alphas)
     Dr = zeros(N, 3);
     for j = 1:N
-        Dr(t, :) = [eta*cos(omega*x(2)*cos(alphas(j)) + omega*x(3)*sin(alphas(j))), ...
+        Dr(j, :) = [eta*cos(omega*x(2)*cos(alphas(j)) + omega*x(3)*sin(alphas(j))), ...
             -x(1)*omega*cos(alphas(j))*eta*sin(omega*x(2)*cos(alphas(j)) + omega*x(3)*sin(alphas(j))), ...
             -x(1)*omega*sin(alphas(j))*eta*sin(omega*x(2)*cos(alphas(j)) + omega*x(3)*sin(alphas(j)))];
-        
-        [1, t, sin(2*pi*t/x(5)), cos(2*pi*t/x(5)), ...
-        (2*pi*t/(x(5)^2))*(x(4)*sin(2*pi*t/x(5))-x(3)*cos(2*pi*t/x(5)))];
     end
 end
 
-function x = gaussNewton(k, N, eta, omega, alphas)
-    x = [0.9, 0.5, 0.3]';
+function x = gaussNewton(k, N, eta, omega, alphas, Ic_alphas)
+    x = [1.05, 0.4, 0.25]';
     for i=1:k
         A = derivative_r(x, N, eta, omega, alphas);
-        v = (A')*A \ -(A')*func_r(x, N, eta, omega, alphas);
+        cond_num = cond(A' * A);
+        if cond_num > 1e10
+            warning('Matrix (A'' * A) is ill-conditioned. Results may be inaccurate.');
+        end
+        v = (A')*A \ -(A')*func_r(x, N, eta, omega, alphas, Ic_alphas);
         x = x + v;
     end
 end
 
-disp(B)
-MAX_ALPHA = 10;
-for i=1:MAX_ALPHA
-    alpha = (i/MAX_ALPHA).*2*pi;
+eta = trapInt2D(200, nuIntegrand, omega);
+disp(eta);
 
+[B,Sol] = hhsolver(omega,S,100); 
+
+MAX_ALPHA = 100;
+alphas = linspace(0, 2*pi, MAX_ALPHA);
+Ic_alphas = zeros(length(alphas), 1);
+
+noiselevel = 3;
+gnoise = B.un+max(abs(B.un))*randn(size(B.un))*noiselevel;
+
+for i=1:length(alphas)
     % Calculate v_c test wave vals for boundary points
-    vc_vals = v_c(B.x, B.y, alpha, omega);
+    vc_vals_i = v_c(B.x, B.y, alphas(i), omega);
 
     % Integrand for I_c(alpha)
-    integrand = B.un .* vc_vals;
-    I_c_alpha = numInt(B.s, integrand);
+    integrand_i = gnoise .* vc_vals_i;
+    Ic_alpha_i = numInt(B.s, integrand_i);
+    Ic_alphas(i) = Ic_alpha_i;
 
-    disp(I_c_alpha)
-    % To-do finish 3b (GN method to estimate values for a, x_0 and y_0)
-    nonlinear_coeffs = gaussNewton();
-
+    
 end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+disp(Ic_alphas)
+% To-do finish 3b (GN method to estimate values for a, x_0 and y_0)
+nonlinear_coeffs = gaussNewton(10, length(alphas), eta, omega, alphas, Ic_alphas);
+disp(nonlinear_coeffs)
+
+
+% Compute the right-hand side: aa * eta * v_c
+vc_vals = zeros(length(alphas), 1);
+for i = 1:length(alphas)
+    vc_vals(i) = v_c(xs, ys, alphas(i), omega);
+end
+rhs = aa * eta * vc_vals;
+
+figure(1);
+plot(alphas, Ic_alphas, 'b-'); 
+hold on;
+grid on;
+plot(alphas, rhs, 'r--');
+hold off;
+
+figure(2)
+
+surf(Sol.x,Sol.y,Sol.u)
+shading interp
+light                    
+lighting gouraud       % Tar lång tid när N är stor
+view(-60,60)
+
+figure(3)
+%surf(Sol.x,Sol.y,Sol.S)
+surf(Sol.x,Sol.y,S(Sol.x,Sol.y))
+shading interp
+light
+lighting gouraud       % Tar lång tid när N är stor
+colormap('autumn')
+material shiny
+view(-60,60)
+
+figure(4)
+contour(Sol.x,Sol.y,Sol.u,20)
+axis equal
+hold on
+plot(B.x,B.y,'k-','LineWidth',2)
+[c,hnd]=contour(Sol.x,Sol.y,S(Sol.x,Sol.y),10); %Sol.S,10);
+set(hnd,'Color','k','LineWidth',1.5)
+hold off
+axis off
+
+
+figure(5)
+mesh(Sol.x,Sol.y,Sol.u)
+
+S2 = @(x,y) nonlinear_coeffs(1)*S0((x-nonlinear_coeffs(2)).^2+(y-nonlinear_coeffs(3)).^2);
+[B2,Sol2] = hhsolver(omega,S2,100); 
+
+figure(6)
+
+surf(Sol2.x,Sol2.y,Sol2.u)
+shading interp
+light                    
+lighting gouraud       % Tar lång tid när N är stor
+view(-60,60)
+
+figure(7)
+%surf(Sol.x,Sol.y,Sol.S)
+surf(Sol2.x,Sol2.y,S2(Sol2.x,Sol2.y))
+shading interp
+light
+lighting gouraud       % Tar lång tid när N är stor
+colormap('autumn')
+material shiny
+view(-60,60)
+
+figure(8)
+contour(Sol2.x,Sol2.y,Sol2.u,20)
+axis equal
+hold on
+plot(B2.x,B2.y,'k-','LineWidth',2)
+[c,hnd]=contour(Sol2.x,Sol2.y,S2(Sol2.x,Sol2.y),10); %Sol.S,10);
+set(hnd,'Color','k','LineWidth',1.5)
+hold off
+axis off
+
+
+figure(9)
+mesh(Sol2.x,Sol2.y,Sol2.u)
